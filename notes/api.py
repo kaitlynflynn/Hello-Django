2a94ab5f21646456c163b64cd3b06551e20d64ba
Kaitlyn Flynn <kaitlynflynn@gmail.com>
2018-08-01 10:42:27 -0700

    notes/api.py - Implemented keyword arg/validated_data for user & python debugger

--- notes/api.py
+++ notes/api.py
@@ -4,6 +4,12 @@ from .models import PersonalNote
 class PersonalNoteSerializer(serializers.HyperlinkedModelSerializer): 
     """Describe the model and fields we want to use."""
 
+    def create(self, validated_data): # ** = keyword args
+        # import pdb; pdb.set_trace() # ; = line separator 
+        user = self.context['request'].user
+        note = PersonalNote.objects.create(user=user, **validated_data)
+        return note 
+
     class Meta: # subclass must be named meta
         model = PersonalNote
         fields = ('title', 'content', 'url')

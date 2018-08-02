3133748002fdd08670dce5868199ff40a1980029
Kaitlyn Flynn <kaitlynflynn@gmail.com>
2018-08-02 10:21:10 -0700

    djorg/settings.py - Added DEFAULT_AUTHENTICATION_CLASSES & add rest_framework.authtoken to INSTALLED_APPS

--- djorg/settings.py
+++ djorg/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
@@ -122,8 +123,17 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+from rest_framework.authentication import TokenAuthentication, BasicAuthentication, SessionAuthentication
+
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
+    ],
+
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
     ]
+
 }

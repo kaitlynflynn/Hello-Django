f8609cd19da350936700f0e770c41952da48b47f
Kaitlyn Flynn <kaitlynflynn@gmail.com>
2018-08-03 13:16:15 -0700

    djorg/settings.py - Edited for whitenoise fix; still troubleshooting

--- djorg/settings.py
+++ djorg/settings.py
@@ -143,8 +143,13 @@ REST_FRAMEWORK = {
 
 }
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 # # CONFIGURE DJANGO APP FOR HEROKU
 

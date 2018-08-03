f648f3417b4aaa2ca05935b29a2acb1eda404c4a
Kaitlyn Flynn <kaitlynflynn@gmail.com>
2018-08-03 13:40:04 -0700

    djorg/settings.py - Edited for troubleshooting

--- djorg/settings.py
+++ djorg/settings.py
@@ -27,7 +27,8 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool) #turns into boolean
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS').split(',')
+# ALLOWED_HOSTS = config('ALLOWED_HOSTS').split(',')
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 
 # Application definition

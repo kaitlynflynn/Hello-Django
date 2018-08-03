4d4ce003d6abe8366dbae6ce6205851a5fd0b977
Kaitlyn Flynn <kaitlynflynn@gmail.com>
2018-08-03 11:08:34 -0700

    djorg/settings.py - Added whitenoise middleware & DATABASE_URL config

--- djorg/settings.py
+++ djorg/settings.py
@@ -25,7 +25,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', cast=bool) #turns into boolean
+# DEBUG = config('DEBUG', cast=bool) #turns into boolean
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -46,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -123,6 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+DATABASES['DATABASE_URL'] = dj_database_url.config(conn_max_age=600)
 
 from rest_framework.authentication import TokenAuthentication, BasicAuthentication, SessionAuthentication
 

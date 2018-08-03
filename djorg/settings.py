ecc3743e8502fb9f616bec100bdeac1ea4507613
Kaitlyn Flynn <kaitlynflynn@gmail.com>
2018-08-03 13:03:37 -0700

    djorg/settings.py - Edited DATABASES again; heroku deploys!

--- djorg/settings.py
+++ djorg/settings.py
@@ -86,9 +86,8 @@ WSGI_APPLICATION = 'djorg.wsgi.application'
 #     }
 # }
 
-DATABASES = {
-    'default': dj_database_url.config(default=config('DATABASE_URL'))
-}
+DATABASES = {}
+DATABASES['default'] = dj_database_url.config('DATABASE_URL', conn_max_age=600)
 
 
 # Password validation

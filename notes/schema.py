8190da95c01da46dd56a0c3aafa0b3e22a34a0bb
Kaitlyn Flynn <kaitlynflynn@gmail.com>
2018-08-08 14:25:38 -0700

    notes/schema.py - Created new file with graphene_django imports; created classes

---
+++ notes/schema.py
@@ -0,0 +1,25 @@
+from graphene_django import DjangoObjectType
+import graphene
+from .models import PersonalNote as PersonalNoteModel 
+
+class PersonalNote(DjangoObjectType):
+
+    class Meta:
+        model = PersonalNoteModel 
+        # Describe the data as a node in the graph
+        interfaces = (graphene.relay.Node,)
+
+class Query(graphene.ObjectType):
+    personalnotes = graphene.List(PersonalNote)
+
+    def resolve_personalnotes(self, info):
+        """Decide which notes to return.""" 
+
+        user = info.context.user # Find with debugger
+
+        if user.is_anonymous: 
+            return PersonalNoteModel.objects.none()
+        else:
+            return PersonalNoteModel.objects.filter(user=user)
+
+schema = graphene.Schema(query=Query)
